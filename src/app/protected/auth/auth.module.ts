2edfbb07df6d28c00aa2ab1c1243a62584008d02
pragith98 <lakshanxp@hotmail.com>
2023-08-15 01:51:55 +0530

    refactor: remove loging folder and put login-component files in to auth folder

--- src/app/protected/auth/auth.module.ts
+++ src/app/protected/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //angular material modules
